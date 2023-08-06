4812d535cef45543dde254a39ba3c1eaa55c10dc
Liam Collod <lcollod@gmail.com>
2023-08-06 13:26:54 +0200

    add `disable_colorspaces` context manager
    closes #1

--- cocoon/colorspaces/datasets/browse.py
+++ cocoon/colorspaces/datasets/browse.py
@@ -5,10 +5,70 @@ __all__ = (
     "get_colorspace",
 )
 
+import contextlib
 from typing import Optional
 
 from cocoon.colorspaces import RgbColorspace
 from cocoon.colorspaces.datasets._build import _COLORSPACES
+
+
+_COLORSPACES_KEYS_DISABLED: list[str] = []
+
+
+def _colorspaces_dataset():
+    """
+    Get the dataset of enabled colorspace.
+    """
+    # not a deepcopy, we want to keep the same instances
+    dataset = _COLORSPACES.copy()
+    [dataset.pop(key) for key in _COLORSPACES_KEYS_DISABLED]
+    return dataset
+
+
+@contextlib.contextmanager
+def disable_colorspaces(names: list[str]):
+    """
+    Make the given colorspaces innacessible via the get functions.
+
+    Nesting context is cummulative (disable upon the previously disable colorspaces).
+
+    Examples:
+
+        >>> import cocoon
+        >>> assert cocoon.get_colorspace("sRGB") is not None
+        >>> with disable_colorspaces(["sRGB"]):
+        >>>     assert cocoon.get_colorspace("sRGB") is None
+        >>> assert cocoon.get_colorspace("sRGB") is not None
+
+    Args:
+        names: list of valid colorspaces names/alias to disable
+    """
+    global _COLORSPACES_KEYS_DISABLED
+    previous_disabled = _COLORSPACES_KEYS_DISABLED.copy()
+
+    for colorspace_name in names:
+        colorspace = _COLORSPACES[colorspace_name]
+        aliases = _get_colorspace_aliases(colorspace)
+        _COLORSPACES_KEYS_DISABLED.extend(aliases)
+
+    try:
+        yield
+    finally:
+        _COLORSPACES_KEYS_DISABLED = previous_disabled
+
+
+def _get_colorspace_aliases(colorspace: RgbColorspace) -> list[str]:
+    """
+    Get all the aliases name that allow to retrive the given colorspace.
+    """
+    aliases = []
+
+    for colorspace_item_name, colorspace_item in _colorspaces_dataset().items():
+        if colorspace != colorspace_item:
+            continue
+        aliases.append(colorspace_item_name)
+
+    return aliases
 
 
 def get_available_colorspaces() -> list[RgbColorspace]:
@@ -20,7 +80,7 @@ def get_available_colorspaces() -> list[RgbColorspace]:
         return colorspace.name
 
     return sorted(
-        list(set(_COLORSPACES.values())),
+        list(set(_colorspaces_dataset().values())),
         key=get_colorspace_name,
     )
 
@@ -30,7 +90,7 @@ def get_available_colorspaces_names() -> list[str]:
     List of colorspace indentifier that correspond to a colorspace.
     No duplicates and sorted alphabetically by name.
     """
-    return sorted(list(set(_COLORSPACES.keys())))
+    return sorted(list(set(_colorspaces_dataset().keys())))
 
 
 def get_available_colorspaces_names_aliases() -> list[tuple[str]]:
@@ -48,7 +108,7 @@ def get_available_colorspaces_names_aliases() -> list[tuple[str]]:
 
     buffer_dict = {}
 
-    for colorspace_name, colorspace in _COLORSPACES.items():
+    for colorspace_name, colorspace in _colorspaces_dataset().items():
         identifier = hash(colorspace)
 
         if buffer_dict.get(identifier):
@@ -80,7 +140,7 @@ def get_colorspace(
     if not name:
         return None
 
-    colorspace = _COLORSPACES.get(name)
+    colorspace = _colorspaces_dataset().get(name)
     if not colorspace:
         return None
 
--- tests/test_colorspaces_datasets.py
+++ tests/test_colorspaces_datasets.py
@@ -1,4 +1,5 @@
 from cocoon import colorspaces
+import cocoon.colorspaces.datasets.browse
 
 
 def test_get_colorspace():
@@ -35,3 +36,47 @@ def test_get_colorspace_linear():
 
     result = colorspaces.get_colorspace("DCI-P3-P", force_linear=True)
     assert result.transfer_functions == colorspaces.TRANSFER_FUNCTIONS_LINEAR
+
+
+def test_disable_colorspaces():
+    colorspace_acescg = colorspaces.get_colorspace("ACEScg")
+    assert colorspace_acescg is not None
+    assert colorspace_acescg in colorspaces.get_available_colorspaces()
+    assert colorspaces.get_colorspace("ap1") is colorspace_acescg
+    assert colorspaces.get_colorspace("acescg") is colorspace_acescg
+    assert colorspaces.get_colorspace("sRGB") is not None
+    assert colorspaces.get_colorspace("Passthrough") is not None
+
+    with cocoon.colorspaces.datasets.browse.disable_colorspaces(
+        ["Passthrough", "ACEScg"]
+    ):
+        assert colorspaces.get_colorspace("ACEScg") is None
+        assert colorspaces.get_colorspace("ap1") is None
+        assert colorspaces.get_colorspace("acescg") is None
+        assert colorspaces.get_colorspace("Passthrough") is None
+        assert colorspaces.get_colorspace("raw") is None
+        assert "ACEScg" not in colorspaces.get_available_colorspaces_names()
+
+    assert colorspaces.get_colorspace("ACEScg") is not None
+    assert colorspaces.get_colorspace("ap1") is not None
+    assert colorspaces.get_colorspace("acescg") is not None
+    assert colorspaces.get_colorspace("Passthrough") is not None
+
+    with cocoon.colorspaces.datasets.browse.disable_colorspaces(
+        ["Passthrough", "ACEScg"]
+    ):
+        with cocoon.colorspaces.datasets.browse.disable_colorspaces(["sRGB"]):
+            assert colorspaces.get_colorspace("ACEScg") is None
+            assert colorspaces.get_colorspace("sRGB") is None
+            assert colorspaces.get_colorspace("Passthrough") is None
+
+    assert colorspaces.get_colorspace("ACEScg") is not None
+    assert colorspaces.get_colorspace("sRGB") is not None
+    assert colorspaces.get_colorspace("Passthrough") is not None
+
+
+def test__get_colorspace_aliases():
+    colorspace = cocoon.sRGB_COLORSPACE
+    result = cocoon.colorspaces.datasets.browse._get_colorspace_aliases(colorspace)
+    expected = ["sRGB Piecewise", "srgb-piecewise", "srgb", "sRGB"]
+    assert result == expected
