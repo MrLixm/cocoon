20c9500681b615e5bca0a1f36035dc370ac45fe5
Liam Collod <lcollod@gmail.com>
2023-08-10 19:38:05 +0200

    add `generate LCHab gradient` function in public interface

--- cocoon/color/__init__.py
+++ cocoon/color/__init__.py
@@ -6,6 +6,9 @@ from ._encodings import convert_float_to_int8
 from ._rgb import RGBAColor
 from ._xyz import XYZColor
 from ._lchab import LCHabColor
+from ._lchab import generate_hab_gradient
+from ._lchab import generate_chroma_gradient
+from ._lchab import generate_lightness_gradient
 from ._rgb_str import ColorStringFormat
 from ._rgb_str import ValidatorResult
 from ._rgb_str import convert_str_to_color
